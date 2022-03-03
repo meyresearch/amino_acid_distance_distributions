9ca592e03d9089bbc1164ded70ca478352daa240
jasmin-guven <jasmin.guven@hotmail.com>
2022-03-03 13:21:18 +0000

    retrieving RCSB pdbs

---
+++ new_repo/scripts/retrieve_rcsb_pdbs.py
@@ -0,0 +1,34 @@
+"""
+Opens each chunked ID file and retrieves PDB files from RCSB to data directory.
+"""
+from Bio.PDB.PDBList import PDBList
+import os
+import pandas as pd
+
+
+def concatenate_id_files():
+    """
+    Opens id_file_N.txt as a DF and concatenates them all into one DF
+    @return:
+    """
+    frames = []
+    for filename in os.listdir("../data/rcsb/ids/"):
+        if filename.endswith(".txt"):
+            temp_df = pd.read_csv(f"../data/rcsb/ids/{filename}", header=None)
+            frames.append(temp_df)
+    dataframe = pd.concat(frames).reset_index()
+    dataframe = dataframe.drop(columns='index')
+    pdb_array = dataframe[0].to_numpy()
+    return pdb_array
+
+
+pdblist = PDBList()
+pdb_ids = concatenate_id_files()
+counter = 1
+for pdb in pdb_ids:
+    print(f"At entry {counter}")
+    pdblist.retrieve_pdb_file(pdb_code=pdb,
+                              file_format="pdb",
+                              pdir="../data/rcsb/pdb_files/")
+    counter += 1
+
