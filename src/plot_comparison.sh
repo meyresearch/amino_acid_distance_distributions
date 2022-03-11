f5feee0d6c6f3e3daeab9a15fa7fdad389632632
jasmin-guven <jasmin.guven@hotmail.com>
2022-03-11 11:20:26 +0000

    updated .gitignore

--- src/plot_comparison.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python plot.py 100 BOTH --A-BS 0.0019 --a-BS 2 --A-C 0.0021 --a-C 3
-python plot.py 200 BOTH --A-BS 0.0012 --a-BS 3 --A-C 0.0011 --a-C 3
-python plot.py 300 BOTH --A-BS 0.0009 --a-BS 4 --A-C 0.0010 --a-C 5
