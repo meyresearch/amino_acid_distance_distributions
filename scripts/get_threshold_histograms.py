50cad6570f760f6d7c3d4de3f6f5fbe77809c145
jasmin-guven <jasmin.guven@hotmail.com>
2022-05-16 10:41:20 +0100

    code for getting threshold hists

--- scripts/get_threshold_histograms.py
+++ scripts/get_threshold_histograms.py
@@ -8,7 +8,7 @@ def return_distance_histogram(log_file: str, given_algorithm: str, length_range:
     Compute the amino acid distance distribution for PDB files in given range from adjacency matrix
     and save in a numpy file.
     @param log_file: file to save exceptions in
-    @param given_algorithm: alpha or rcsb
+    @param given_algorithm: alphafold or rcsb
     @param length_range: 100, 200 or 300
     @param path_to_csvs: full path to csv files
     @param threshold: threshold value to use for counting contacts
@@ -21,7 +21,7 @@ def return_distance_histogram(log_file: str, given_algorithm: str, length_range:
     with open(log_file, "w") as log_file:
         for pdb_file in pdb_files:
             # print(f"Progress: {counter}/{len(pdb_files)}")
-            if given_algorithm == "alpha":
+            if given_algorithm == "alphafold":
                 clean_pdb_filename = pdb_file.replace("/home/jguven/Projects/sequence_distance_distribution", "..")
             else:
                 clean_pdb_filename = pdb_file
@@ -38,13 +38,19 @@ def return_distance_histogram(log_file: str, given_algorithm: str, length_range:
     histogram_array = np.asarray(histogram_list)
     if not histogram_list:
         print("Warning: Histogram list is empty. Check log file.")
-    if given_algorithm == "alpha":
-        np.save(f"histogram_alpha_{length_range}_{threshold}.npy", histogram_array)
+    if given_algorithm == "alphafold":
+        np.save(f"histogram_alphafold_{length_range}_{threshold}.npy", histogram_array)
     elif given_algorithm == "rcsb":
         np.save(f"histogram_{length_range}_{threshold}.npy", histogram_array)
 
 
-length = 300
-algorithm = "rcsb"
-
-#return_distance_histogram(log_file="log.txt", given_algorithm=algorithm, length_range=str{length}, path_to_csvs=f"../data/rcsb/secondary_structures_{length}.csv")
+lengths = [100, 200, 300]
+algorithms = ["rcsb", "alphafold"]
+filename = ""
+for algorithm in algorithms:
+    if algorithm == "alphafold":
+        filename = f"unique_secondary_structures_{length}.csv"
+    elif algorithm == "rcsb":
+        filename = f"secondary_structures_{length}.csv"
+    for length in lengths:
+    return_distance_histogram(log_file="log.txt", given_algorithm=algorithm, length_range=str{length}, path_to_csvs=f"../data/{algorithm}/{filename}")
