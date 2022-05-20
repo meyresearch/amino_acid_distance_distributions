43a852e9565472b8a0d0b4260378fc2764feccb4
jasmin-guven <jasmin.guven@hotmail.com>
2022-05-20 11:35:21 +0100

    final plots for arxiv submission

--- scripts/adjacency.py
+++ scripts/adjacency.py
@@ -38,6 +38,19 @@ def set_adjacency_matrix_ticks(plot: matplotlib.axes.Axes) -> None:
             label.set_visible(False)
 
 
+def remove_diagonal_elements(matrix: np.ndarray):
+    """
+    Remove diagonal elements and add contacts to first off-diagonals
+    @param matrix: adjacency matrix from simulations
+    @return: simulation matrix with diagonals set to 1
+    """
+    np.fill_diagonal(matrix, 0)
+    off_diagonal = np.ones(len(np.diag(matrix, 1)))
+    np.fill_diagonal(matrix[1:], off_diagonal)
+    np.fill_diagonal(matrix[:, 1:], off_diagonal)
+    return matrix
+
+
 def plot_adjacency_matrix(file: str, data_type: str) -> None:
     """
     Plot adjacency matrix for given protein file (or simulation matrix)
@@ -47,9 +60,11 @@ def plot_adjacency_matrix(file: str, data_type: str) -> None:
     """
     adjacency_matrix = []
     if data_type == "sim":
-        adjacency_matrix = functions.get_3d_simulation_adjacency_matrix(file)
+        simulation_matrix = functions.get_3d_simulation_adjacency_matrix(file)
+        adjacency_matrix = remove_diagonal_elements(simulation_matrix)
     elif data_type == "pdb":
-        adjacency_matrix = get_pdb_matrix(file)
+        pdb_matrix = get_pdb_matrix(file)
+        adjacency_matrix = remove_diagonal_elements(pdb_matrix)
     plt.figure(figsize=(6, 6))
     sns.set(context="notebook", palette="colorblind", style="ticks", font_scale=1.8, font="Helvetica")
     colormap = [_COLOUR_PALETTE["NO_CONTACT"], _COLOUR_PALETTE["CONTACT"]]
