4957db098239f1f831ccbfd448f59819eacb2d5c
jasmin-guven <jasmin.guven@hotmail.com>
2022-11-25 10:55:59 +0000

    code for getting average adjacency matrices and low-confidence AF2 structures

--- scripts/adjacency.py
+++ scripts/adjacency.py
@@ -76,3 +76,39 @@ def plot_adjacency_matrix(file: str, data_type: str) -> None:
     plt.tight_layout()
     plt.savefig(f"../plots/adjacency_matrices/{data_type}_matrix.jpeg", dpi=900)
     plt.show()
+
+
+# skeleton code for average matrix
+
+# read in secondary_structure_{range}.csv
+# dataframe = pd.read_csv(path_to_csv) 
+# pdb_filenames = dataframe["filename"].tolist()
+
+# Getting adjacency matrix data:
+# with open("log.txt", "w") as log_file:
+#   for pdb_filename in pdb_filenames:
+        # given_algortihm = [rcsb/alpha]
+#       if given_algorithm == "alpha":
+#               clean_pdb_filename = pdb_file.replace("/home/jguven/Projects/sequence_distance_distribution", "..")
+#       else:
+#               clean_pdb_filename = pdb_filename
+#       try:
+#           adjacency_matrix = functions.adjacency_matrix(clean_pdb_filename)[1]
+#           all_adjacency_matrices_list.append(adjacency_matrix)
+#       except FileNotFoundError:
+#           traceback.print_exc(file=log_file)
+#       all_adjacency_matrices = np.array(all_adjacency_matrices_list)
+#    if not histogram_list:
+#        print("Warning: Histogram list is empty. Check log file.")
+#   if given_algorithm == "alpha":
+#        np.save(f"../data/alphafold/adjacency_matrix_{length_range}.npy", all_adjacency_matrices)
+#   elif given_algorithm == "rcsb":
+#        np.save(f"../data/rcsb/adjacency_matrix_{length_range}.npy", all_adjacency_matrices)
+       
+# Get average adjacency matrix:
+
+# total_matrix = np.load(f"../data/rcsb/adjacency_matrix_{length_range}.npy")
+# n_total_counts = np.sum(total_matrix)
+# average_adjacency_matrix = np.sum(total_matrix, axis=0) / n_total_counts
+
+# plot
--- scripts/confidences.py
+++ scripts/confidences.py
@@ -1,6 +1,7 @@
 import glob
 import pandas as pd
 import numpy as np
+import argparse
 
 
 def get_confidences(pdb_files: list) -> list:
@@ -31,19 +32,63 @@ def get_confidences(pdb_files: list) -> list:
     return confidence_info
                                           
                                       
-def filter_confidences(pdb_files: list) -> pd.DataFrame:
+def filter_confidences(pdb_files: list, low_confidence: bool) -> pd.DataFrame:
     """
     Filter out AlphaFold structures with less than 90% confidence
     @param pdb_files: glob list of AlphaFold PDBs
-    @return good_confidence_df: dataframe containing PDBs with 90% confidence or above
+    @param low_confidence: True: get confidences below 90.0, False: get confidences above 90.0
+    @return good_confidence_df: dataframe containing PDBs with either low or high confidence
     """
     confidence_list = get_confidences(pdb_files)
     confidence_dataframe = pd.DataFrame.from_dict(confidence_list)
-    confidence_filter = confidence_dataframe["mean"] > 90.0
-    good_confidence_df = confidence_dataframe.where(confidence_filter).dropna()
-    return good_confidence_df
+    if low_confidence: 
+        confidence_filter = confidence_dataframe["mean"] <= 90.0
+    elif not low_confidence:
+        confidence_filter = confidence_dataframe["mean"] > 90.0
+    return confidence_dataframe.where(confidence_filter).dropna()
 
 
-pdbs = glob.glob("../data/alphafold/pdb_files/*.pdb")
-confidence_df = dssp_functions.filter_confidences(pdbs)
-confidence_df.to_csv("../data/alphafold/confidences.csv")
+def check_positive(input) -> float:
+    """
+    Type-checking for per-residue confidence scores
+    @param input: user input from command-line arguments
+    @return: checked per-residue confidence score
+    """
+    try:
+        confidence = float(input)
+        if confidence <= 0:
+            raise argparse.ArgumentTypeError(f"{confidence} is an invalid per-residue confidence score")
+    except ValueError:
+        print("Error: per-residue confidence score should be a number")
+    except argparse.ArgumentTypeError as message:
+        print(message)
+    return confidence
+
+
+def commandline_arguments() -> argparse.Namespace:
+    """
+    Parser for commandline arguments
+    @return: commandline arguments from user
+    """
+    parser = argparse.ArgumentParser(description="filter AlphaFold 2 structures based on per-residue confidence scores")
+    parser.add_argument("-l",
+                        "--low",
+                        help="get low-confidence AlphaFold 2 structures, default behaviour is to only get structures with confidence above 90",
+                        action="store_true")
+    return parser.parse_args()
+
+
+def main():
+    arguments = commandline_arguments()
+    low_confidence = arguments.low
+    pdbs = glob.glob("../data/alphafold/pdb_files/*.pdb")
+    confidence_df = filter_confidences(pdbs, low_confidence)
+    if low_confidence:
+        savename = "low_confidences.csv"
+    else:
+        savename = "confidences.csv"
+    confidence_df.to_csv(f"../data/alphafold/{savename}")
+
+
+if __name__ == "__main__":
+    main()
